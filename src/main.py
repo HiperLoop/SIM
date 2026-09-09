13c4ac2ddaa7a771de1c98f21b9dff2410b36d3b
Adam Zich <adam.zich3@gmail.com>
2026-09-09 16:51:03 +0200

    updated code to only use the reduced temperature

--- src/main.py
+++ src/main.py
@@ -3,11 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 rng = np.random.default_rng()
 
-J:float = 1 #Coupling constant
-T:float = 1 #Temperature
-k_B:float = const.Boltzmann #Boltzman constant
-T_red:float = (k_B * T) / J #Reduced temperature
-min_BT: float = -1/(k_B * T) #useful value for calculating flip probabilities
+T_red = 1 #Reduced temperature
 n:int = 20 #Sqrt of number of spins
 N:int = n*n #Number of spins
 
@@ -21,25 +17,25 @@ def generate_initial_spin_orientations(n: int, down_probability: float):
     random_valued_matrix[random_valued_matrix >= down_probability] = 1
     return random_valued_matrix.astype(int)
 
-def get_energy_at_position( J: float, n: int, position: int[2], spin_matrix: int[n][n]):
+def get_reduced_energy_difference_at_position(n: int, position: int[2], spin_matrix: int[n][n]):
     x, y = position
     nearest_neighbour_sum = spin_matrix[(x+1)%n][y] + spin_matrix[(x-1)%n][y] + spin_matrix[x][(y+1)%n] + spin_matrix[x][(y-1)%n]
-    return -J * spin_matrix[x][y] * nearest_neighbour_sum
+    return 2 * spin_matrix[x][y] * nearest_neighbour_sum
 
-def metropolis_algorithm_step(J: float, n: int, spin_matrix: int[n][n]):
+def metropolis_algorithm_step(n: int, spin_matrix: int[n][n]):
     random_position: int[2] = rng.integers(0, n, size = 2)
-    flip_energy = get_energy_at_position(J, n, random_position, spin_matrix)
-    if flip_energy <= 0 or rng.random() < np.exp(flip_energy * min_BT):
+    flip_energy = get_reduced_energy_difference_at_position(n, random_position, spin_matrix)
+    if flip_energy <= 0 or rng.random() < np.exp(-flip_energy / T_red):
         spin_matrix[random_position[0]][random_position[1]]*=-1
 
-def simulation(random_seed: int, J: float, n: int, down_probability: float, iterations: int):
+def simulation(random_seed: int, n: int, down_probability: float, iterations: int):
     np.random.seed(random_seed)
     spin_matrix: int[n][n] = generate_initial_spin_orientations(n, down_probability)
     for _ in range(iterations):
-        metropolis_algorithm_step(J, n, spin_matrix)
+        metropolis_algorithm_step(n, spin_matrix)
 
 def main():
-    simulation(1, J, n, 0.5, 10)
+    simulation(1, n, 0.5, 10)
 
 if __name__=="__main__":
     main()
